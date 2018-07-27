8d46d37a0f12c91f6d4b17e3d4c0424b83befc65
mgillam <mic@secureideas.com>
2018-07-27 10:11:17 -0400

    WIP - gen-options target multiselect based on installers directory and default-options

--- lib/vagrant-yaml-options/gen-options.rb
+++ lib/vagrant-yaml-options/gen-options.rb
@@ -1,4 +1,7 @@
 require 'vagrant'
+require 'tty-prompt'
+require 'yaml'
+
 module VagrantYamlOptions
   class GenerateOptions < Vagrant.plugin('2', :command)
     def self.synopsis
@@ -8,12 +11,35 @@ module VagrantYamlOptions
     def initialize(argv, env)
       @argv = argv
       @env = env
+      @prompt = TTY::Prompt.new
       @cmd_name = 'gen-options'
     end
 
     def execute
-      name = @env.ui.ask('What is your name?')
-      @env.ui.say("Hello, #{name}!")
+      @env.ui.say(:info, "Welcome to the SamuraiWTF target selector!")
+      all_targets = Dir.glob("target_install/*").map { |name| name[15..-4] }
+      default_options = YAML.load_file('default-options.yaml')
+
+      default_indices = []
+      all_targets.each_with_index { |name, index|
+          if default_options['targets'].index(name) != nil
+              default_indices.push(index + 1)
+          end
+      }
+
+      @env.ui.say(:info, default_indices)
+
+      selected_targets = @prompt.multi_select("Select targets (space to toggle, enter when done)", echo: false) do |menu|
+        menu.default(*default_indices)
+
+        all_targets.each do |target|
+          menu.choice target
+        end
+      end
+
+      # choices = all_targets #default_options['targets']
+      # selected_targets = @prompt.multi_select("Select your targets (enter when done)", choices)
+      @env.ui.say(:info, "Chose: #{selected_targets}")
     end
 
   end
